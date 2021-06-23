d8ffdaf519da4fb050a80e977caa50ca663a5921
rodrigo <68453000+andresico01@users.noreply.github.com>
2021-06-23 08:45:57 -0500

    Update Crear.java

--- src/main/java/com/accenture/crear/Crear.java
+++ src/main/java/com/accenture/crear/Crear.java
@@ -1,35 +1,66 @@
 package com.accenture.crear;
 
+import com.accenture.dao.InstanceDao;
+import com.accenture.dao.QuoteDao;
 import com.accenture.hibernate.Instance;
 import com.accenture.hibernate.Quote;
-import net.bytebuddy.pool.TypePool;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public class Crear {
-    private static final String PU = "HibernateJpaMYSQLPU";
+
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory(PU);
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
+
+        InstanceDao newInstance = new InstanceDao();
+        QuoteDao newQuote = new QuoteDao();
 
         Instance instance = new Instance();
-        instance.setOffNet("hola");
-        instance.setType("tipo");
+        Instance instance2 = new Instance();
+
         Quote quote = new Quote();
-        quote.setCreateDate("2040-12-1");
-        quote.setOrderNumber("order");
+        Quote quote2 = new Quote();
+
+        quote.setCreateDate(LocalDate.of(2003,10,28));
+        quote.setOrderNumber("1789926638");
+        quote.setUseDate(LocalDate.of(2011,9,17));
+
+        quote2.setCreateDate(LocalDate.of(2011,5,2));
+        quote2.setOrderNumber("768964357");
+        quote2.setUseDate(LocalDate.of(2015,11,1));
+
+        instance.setOffNet("off");
+        instance.setType("metro");
         instance.setQuoteNumeber(quote);
 
-        em.persist(instance);
-        em.persist(quote);
-        em.getTransaction().commit();
+        instance2.setOffNet("off");
+        instance2.setType("Fibra");
+        instance2.setQuoteNumeber(quote2);
 
-        em.close();
-        emf.close();
+        newQuote.insert(quote);
+        newInstance.insert(instance);
 
+        newQuote.insert(quote2);
+        newInstance.insert(instance2);
+
+
+
+        System.out.println("Quote");
+        System.out.println(newQuote.list());
+
+        System.out.println("Instance");
+        System.out.println(newInstance.list());
+
+        newInstance.close();
+        newQuote.close();
+
+
+    }
+    private static void  print(List collection){
+        for (Object aux: collection) {
+            System.out.println(aux.toString());
+        }
 
     }
 }
